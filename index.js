1160bce9bab59be1170db98575c947c0811e4429
Sahil Aggarwal <aggarwalsahil100@gmail.com>
2017-10-20 19:08:12 +0530

    Simple Node example

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter:(x,y) => (2*(x+y)),
+    area:(x,y) =>(x*y)
+};
+
+
+function solveRect(l,b){
+    if(l<=0 || b<=0){
+        console.log("Not Applicable");
+    }
+
+    else{
+        console.log(rect.area(l,b));
+        console.log(rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(0,5);
+solveRect(-3,5);
