d43df4050644e8675321aeb167fa8901ff189532
Sahil Aggarwal <aggarwalsahil100@gmail.com>
2017-10-21 01:09:19 +0530

    Callbacks,closures and error handlng

--- index.js
+++ index.js
@@ -7,14 +7,29 @@ var rect  =require('./rectangle');
 
 
 function solveRect(l,b){
-    if(l<=0 || b<=0){
-        console.log("Not Applicable");
-    }
 
-    else{
-        console.log(rect.area(l,b));
-        console.log(rect.perimeter(l,b));
-    }
+    console.log("Solving for rectangle with l = "
+    + l + " and b = " + b);
+        rect(l,b, (err,rectangle) => {
+        if (err) {
+        console.log("ERROR: ", err.message);
+        }
+        else {
+        console.log("The area of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.area());
+        console.log("The perimeter of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+});
+console.log("This statement after the call to rect()");
+    // if(l<=0 || b<=0){
+    //     console.log("Not Applicable");
+    // }
+
+    // else{
+    //     console.log(rect.area(l,b));
+    //     console.log(rect.perimeter(l,b));
+    // }
 }
 
 solveRect(2,4);
